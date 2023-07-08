b7adccd0844017867d74dd89f098f38c66c84a8c
mohammadJahid8 <mohammadjahid0007@gmail.com>
2023-07-08 22:25:20 +0600

    [Done] Product slice

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -21,15 +21,9 @@ import {
 } from '@/redux/features/cart/cartSlice';
 
 export default function Cart() {
-  const { products } = useAppSelector((state) => state.cart);
+  const { products, total } = useAppSelector((state) => state.cart);
 
   const dispatch = useAppDispatch();
-
-  //! Dummy data
-
-  const total = 0;
-
-  //! **
 
   return (
     <Sheet>
